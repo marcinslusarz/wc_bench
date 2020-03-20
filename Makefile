a8d655aea2837aeaa08b580aebe69eb89b11a12f
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-03-20 10:46:15 +0100

    use all registers in avx/avx512f variants

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ PMEM?=/dev/shm/aaa
 NUMANODE?=0
 CLS?=1
 CLSMAX?=16
+CFLAGS=-DUSE_ALL_REGS -O2 $(shell pkg-config --cflags libpmem)
+LDFLAGS=$(shell pkg-config --libs libpmem)
+LIBDIR=$(shell pkg-config --variable libdir libpmem)
 
 all: wc_sse2 wc_avx wc_avx512f
 
@@ -14,19 +17,19 @@ clean:
 	rm -f wc_sse2 wc_avx wc_avx512f
 
 wc_sse2: wc.c Makefile
-	$(CC) wc.c -DUSE_SSE2 $(shell pkg-config --cflags libpmem) -o wc_sse2 $(shell pkg-config --libs libpmem) -O2
+	$(CC) wc.c -DUSE_SSE2    $(CFLAGS)           -o wc_sse2    $(LDFLAGS)
 
 wc_avx: wc.c Makefile
-	$(CC) wc.c -DUSE_AVX $(shell pkg-config --cflags libpmem) -o wc_avx $(shell pkg-config --libs libpmem) -mavx -O2
+	$(CC) wc.c -DUSE_AVX     $(CFLAGS) -mavx     -o wc_avx     $(LDFLAGS)
 
 wc_avx512f: wc.c Makefile
-	$(CC) wc.c -DUSE_AVX512F $(shell pkg-config --cflags libpmem) -o wc_avx512f $(shell pkg-config --libs libpmem) -mavx512f -O2
+	$(CC) wc.c -DUSE_AVX512F $(CFLAGS) -mavx512f -o wc_avx512f $(LDFLAGS)
 
 run_sse2: wc_sse2
-	LD_LIBRARY_PATH=$(shell pkg-config --variable libdir libpmem):$LD_LIBRARY_PATH numactl -N $(NUMANODE) ./wc_sse2 $(PMEM) $(CLS) $(CLSMAX)
+	LD_LIBRARY_PATH=$(LIBDIR):$LD_LIBRARY_PATH numactl -N $(NUMANODE) ./wc_sse2 $(PMEM) $(CLS) $(CLSMAX)
 
 run_avx: wc_avx
-	LD_LIBRARY_PATH=$(shell pkg-config --variable libdir libpmem):$LD_LIBRARY_PATH numactl -N $(NUMANODE) ./wc_avx $(PMEM) $(CLS) $(CLSMAX)
+	LD_LIBRARY_PATH=$(LIBDIR):$LD_LIBRARY_PATH numactl -N $(NUMANODE) ./wc_avx $(PMEM) $(CLS) $(CLSMAX)
 	
 run_avx512f: wc_avx512f
-	LD_LIBRARY_PATH=$(shell pkg-config --variable libdir libpmem):$LD_LIBRARY_PATH numactl -N $(NUMANODE) ./wc_avx512f $(PMEM) $(CLS) $(CLSMAX)
+	LD_LIBRARY_PATH=$(LIBDIR):$LD_LIBRARY_PATH numactl -N $(NUMANODE) ./wc_avx512f $(PMEM) $(CLS) $(CLSMAX)
--- wc.c
+++ wc.c
@@ -21,20 +21,6 @@
 static const char mode[] = "SSE2";
 
 static inline void
-memmove_movnt1x64b(char *dest, const char *src)
-{
-	__m128i xmm0 = _mm_loadu_si128((__m128i *)src + 0);
-	__m128i xmm1 = _mm_loadu_si128((__m128i *)src + 1);
-	__m128i xmm2 = _mm_loadu_si128((__m128i *)src + 2);
-	__m128i xmm3 = _mm_loadu_si128((__m128i *)src + 3);
-
-	_mm_stream_si128((__m128i *)dest + 0, xmm0);
-	_mm_stream_si128((__m128i *)dest + 1, xmm1);
-	_mm_stream_si128((__m128i *)dest + 2, xmm2);
-	_mm_stream_si128((__m128i *)dest + 3, xmm3);
-}
-
-static inline void
 memmove_movnt4x64b(char *dest, const char *src)
 {
 	__m128i xmm0 = _mm_loadu_si128((__m128i *)src + 0);
@@ -72,9 +58,83 @@ memmove_movnt4x64b(char *dest, const char *src)
 	_mm_stream_si128((__m128i *)dest + 15, xmm15);
 }
 
+static inline void
+memmove_movnt2x64b(char *dest, const char *src)
+{
+	__m128i xmm0 = _mm_loadu_si128((__m128i *)src + 0);
+	__m128i xmm1 = _mm_loadu_si128((__m128i *)src + 1);
+	__m128i xmm2 = _mm_loadu_si128((__m128i *)src + 2);
+	__m128i xmm3 = _mm_loadu_si128((__m128i *)src + 3);
+	__m128i xmm4 = _mm_loadu_si128((__m128i *)src + 4);
+	__m128i xmm5 = _mm_loadu_si128((__m128i *)src + 5);
+	__m128i xmm6 = _mm_loadu_si128((__m128i *)src + 6);
+	__m128i xmm7 = _mm_loadu_si128((__m128i *)src + 7);
+
+	_mm_stream_si128((__m128i *)dest + 0, xmm0);
+	_mm_stream_si128((__m128i *)dest + 1, xmm1);
+	_mm_stream_si128((__m128i *)dest + 2, xmm2);
+	_mm_stream_si128((__m128i *)dest + 3, xmm3);
+	_mm_stream_si128((__m128i *)dest + 4, xmm4);
+	_mm_stream_si128((__m128i *)dest + 5, xmm5);
+	_mm_stream_si128((__m128i *)dest + 6, xmm6);
+	_mm_stream_si128((__m128i *)dest + 7, xmm7);
+}
+
+static inline void
+memmove_movnt1x64b(char *dest, const char *src)
+{
+	__m128i xmm0 = _mm_loadu_si128((__m128i *)src + 0);
+	__m128i xmm1 = _mm_loadu_si128((__m128i *)src + 1);
+	__m128i xmm2 = _mm_loadu_si128((__m128i *)src + 2);
+	__m128i xmm3 = _mm_loadu_si128((__m128i *)src + 3);
+
+	_mm_stream_si128((__m128i *)dest + 0, xmm0);
+	_mm_stream_si128((__m128i *)dest + 1, xmm1);
+	_mm_stream_si128((__m128i *)dest + 2, xmm2);
+	_mm_stream_si128((__m128i *)dest + 3, xmm3);
+}
+
 #elif defined(USE_AVX)
 
 static const char mode[] = "AVX";
+
+static inline void
+memmove_movnt8x64b(char *dest, const char *src)
+{
+	__m256i ymm0 = _mm256_loadu_si256((__m256i *)src + 0);
+	__m256i ymm1 = _mm256_loadu_si256((__m256i *)src + 1);
+	__m256i ymm2 = _mm256_loadu_si256((__m256i *)src + 2);
+	__m256i ymm3 = _mm256_loadu_si256((__m256i *)src + 3);
+	__m256i ymm4 = _mm256_loadu_si256((__m256i *)src + 4);
+	__m256i ymm5 = _mm256_loadu_si256((__m256i *)src + 5);
+	__m256i ymm6 = _mm256_loadu_si256((__m256i *)src + 6);
+	__m256i ymm7 = _mm256_loadu_si256((__m256i *)src + 7);
+	__m256i ymm8 = _mm256_loadu_si256((__m256i *)src + 8);
+	__m256i ymm9 = _mm256_loadu_si256((__m256i *)src + 9);
+	__m256i ymm10 = _mm256_loadu_si256((__m256i *)src + 10);
+	__m256i ymm11 = _mm256_loadu_si256((__m256i *)src + 11);
+	__m256i ymm12 = _mm256_loadu_si256((__m256i *)src + 12);
+	__m256i ymm13 = _mm256_loadu_si256((__m256i *)src + 13);
+	__m256i ymm14 = _mm256_loadu_si256((__m256i *)src + 14);
+	__m256i ymm15 = _mm256_loadu_si256((__m256i *)src + 15);
+
+	_mm256_stream_si256((__m256i *)dest + 0, ymm0);
+	_mm256_stream_si256((__m256i *)dest + 1, ymm1);
+	_mm256_stream_si256((__m256i *)dest + 2, ymm2);
+	_mm256_stream_si256((__m256i *)dest + 3, ymm3);
+	_mm256_stream_si256((__m256i *)dest + 4, ymm4);
+	_mm256_stream_si256((__m256i *)dest + 5, ymm5);
+	_mm256_stream_si256((__m256i *)dest + 6, ymm6);
+	_mm256_stream_si256((__m256i *)dest + 7, ymm7);
+	_mm256_stream_si256((__m256i *)dest + 8, ymm8);
+	_mm256_stream_si256((__m256i *)dest + 9, ymm9);
+	_mm256_stream_si256((__m256i *)dest + 10, ymm10);
+	_mm256_stream_si256((__m256i *)dest + 11, ymm11);
+	_mm256_stream_si256((__m256i *)dest + 12, ymm12);
+	_mm256_stream_si256((__m256i *)dest + 13, ymm13);
+	_mm256_stream_si256((__m256i *)dest + 14, ymm14);
+	_mm256_stream_si256((__m256i *)dest + 15, ymm15);
+}
 
 static inline void
 memmove_movnt4x64b(char *dest, const char *src)
@@ -99,6 +159,20 @@ memmove_movnt4x64b(char *dest, const char *src)
 }
 
 static inline void
+memmove_movnt2x64b(char *dest, const char *src)
+{
+	__m256i ymm0 = _mm256_loadu_si256((__m256i *)src + 0);
+	__m256i ymm1 = _mm256_loadu_si256((__m256i *)src + 1);
+	__m256i ymm2 = _mm256_loadu_si256((__m256i *)src + 2);
+	__m256i ymm3 = _mm256_loadu_si256((__m256i *)src + 3);
+
+	_mm256_stream_si256((__m256i *)dest + 0, ymm0);
+	_mm256_stream_si256((__m256i *)dest + 1, ymm1);
+	_mm256_stream_si256((__m256i *)dest + 2, ymm2);
+	_mm256_stream_si256((__m256i *)dest + 3, ymm3);
+}
+
+static inline void
 memmove_movnt1x64b(char *dest, const char *src)
 {
 	__m256i ymm0 = _mm256_loadu_si256((__m256i *)src + 0);
@@ -114,6 +188,136 @@ memmove_movnt1x64b(char *dest, const char *src)
 static const char mode[] = "AVX512F";
 
 static inline void
+memmove_movnt32x64b(char *dest, const char *src)
+{
+	__m512i zmm0 = _mm512_loadu_si512((__m512i *)src + 0);
+	__m512i zmm1 = _mm512_loadu_si512((__m512i *)src + 1);
+	__m512i zmm2 = _mm512_loadu_si512((__m512i *)src + 2);
+	__m512i zmm3 = _mm512_loadu_si512((__m512i *)src + 3);
+	__m512i zmm4 = _mm512_loadu_si512((__m512i *)src + 4);
+	__m512i zmm5 = _mm512_loadu_si512((__m512i *)src + 5);
+	__m512i zmm6 = _mm512_loadu_si512((__m512i *)src + 6);
+	__m512i zmm7 = _mm512_loadu_si512((__m512i *)src + 7);
+	__m512i zmm8 = _mm512_loadu_si512((__m512i *)src + 8);
+	__m512i zmm9 = _mm512_loadu_si512((__m512i *)src + 9);
+	__m512i zmm10 = _mm512_loadu_si512((__m512i *)src + 10);
+	__m512i zmm11 = _mm512_loadu_si512((__m512i *)src + 11);
+	__m512i zmm12 = _mm512_loadu_si512((__m512i *)src + 12);
+	__m512i zmm13 = _mm512_loadu_si512((__m512i *)src + 13);
+	__m512i zmm14 = _mm512_loadu_si512((__m512i *)src + 14);
+	__m512i zmm15 = _mm512_loadu_si512((__m512i *)src + 15);
+	__m512i zmm16 = _mm512_loadu_si512((__m512i *)src + 16);
+	__m512i zmm17 = _mm512_loadu_si512((__m512i *)src + 17);
+	__m512i zmm18 = _mm512_loadu_si512((__m512i *)src + 18);
+	__m512i zmm19 = _mm512_loadu_si512((__m512i *)src + 19);
+	__m512i zmm20 = _mm512_loadu_si512((__m512i *)src + 20);
+	__m512i zmm21 = _mm512_loadu_si512((__m512i *)src + 21);
+	__m512i zmm22 = _mm512_loadu_si512((__m512i *)src + 22);
+	__m512i zmm23 = _mm512_loadu_si512((__m512i *)src + 23);
+	__m512i zmm24 = _mm512_loadu_si512((__m512i *)src + 24);
+	__m512i zmm25 = _mm512_loadu_si512((__m512i *)src + 25);
+	__m512i zmm26 = _mm512_loadu_si512((__m512i *)src + 26);
+	__m512i zmm27 = _mm512_loadu_si512((__m512i *)src + 27);
+	__m512i zmm28 = _mm512_loadu_si512((__m512i *)src + 28);
+	__m512i zmm29 = _mm512_loadu_si512((__m512i *)src + 29);
+	__m512i zmm30 = _mm512_loadu_si512((__m512i *)src + 30);
+	__m512i zmm31 = _mm512_loadu_si512((__m512i *)src + 31);
+
+	_mm512_stream_si512((__m512i *)dest + 0, zmm0);
+	_mm512_stream_si512((__m512i *)dest + 1, zmm1);
+	_mm512_stream_si512((__m512i *)dest + 2, zmm2);
+	_mm512_stream_si512((__m512i *)dest + 3, zmm3);
+	_mm512_stream_si512((__m512i *)dest + 4, zmm4);
+	_mm512_stream_si512((__m512i *)dest + 5, zmm5);
+	_mm512_stream_si512((__m512i *)dest + 6, zmm6);
+	_mm512_stream_si512((__m512i *)dest + 7, zmm7);
+	_mm512_stream_si512((__m512i *)dest + 8, zmm8);
+	_mm512_stream_si512((__m512i *)dest + 9, zmm9);
+	_mm512_stream_si512((__m512i *)dest + 10, zmm10);
+	_mm512_stream_si512((__m512i *)dest + 11, zmm11);
+	_mm512_stream_si512((__m512i *)dest + 12, zmm12);
+	_mm512_stream_si512((__m512i *)dest + 13, zmm13);
+	_mm512_stream_si512((__m512i *)dest + 14, zmm14);
+	_mm512_stream_si512((__m512i *)dest + 15, zmm15);
+	_mm512_stream_si512((__m512i *)dest + 16, zmm16);
+	_mm512_stream_si512((__m512i *)dest + 17, zmm17);
+	_mm512_stream_si512((__m512i *)dest + 18, zmm18);
+	_mm512_stream_si512((__m512i *)dest + 19, zmm19);
+	_mm512_stream_si512((__m512i *)dest + 20, zmm20);
+	_mm512_stream_si512((__m512i *)dest + 21, zmm21);
+	_mm512_stream_si512((__m512i *)dest + 22, zmm22);
+	_mm512_stream_si512((__m512i *)dest + 23, zmm23);
+	_mm512_stream_si512((__m512i *)dest + 24, zmm24);
+	_mm512_stream_si512((__m512i *)dest + 25, zmm25);
+	_mm512_stream_si512((__m512i *)dest + 26, zmm26);
+	_mm512_stream_si512((__m512i *)dest + 27, zmm27);
+	_mm512_stream_si512((__m512i *)dest + 28, zmm28);
+	_mm512_stream_si512((__m512i *)dest + 29, zmm29);
+	_mm512_stream_si512((__m512i *)dest + 30, zmm30);
+	_mm512_stream_si512((__m512i *)dest + 31, zmm31);
+}
+
+static inline void
+memmove_movnt16x64b(char *dest, const char *src)
+{
+	__m512i zmm0 = _mm512_loadu_si512((__m512i *)src + 0);
+	__m512i zmm1 = _mm512_loadu_si512((__m512i *)src + 1);
+	__m512i zmm2 = _mm512_loadu_si512((__m512i *)src + 2);
+	__m512i zmm3 = _mm512_loadu_si512((__m512i *)src + 3);
+	__m512i zmm4 = _mm512_loadu_si512((__m512i *)src + 4);
+	__m512i zmm5 = _mm512_loadu_si512((__m512i *)src + 5);
+	__m512i zmm6 = _mm512_loadu_si512((__m512i *)src + 6);
+	__m512i zmm7 = _mm512_loadu_si512((__m512i *)src + 7);
+	__m512i zmm8 = _mm512_loadu_si512((__m512i *)src + 8);
+	__m512i zmm9 = _mm512_loadu_si512((__m512i *)src + 9);
+	__m512i zmm10 = _mm512_loadu_si512((__m512i *)src + 10);
+	__m512i zmm11 = _mm512_loadu_si512((__m512i *)src + 11);
+	__m512i zmm12 = _mm512_loadu_si512((__m512i *)src + 12);
+	__m512i zmm13 = _mm512_loadu_si512((__m512i *)src + 13);
+	__m512i zmm14 = _mm512_loadu_si512((__m512i *)src + 14);
+	__m512i zmm15 = _mm512_loadu_si512((__m512i *)src + 15);
+
+	_mm512_stream_si512((__m512i *)dest + 0, zmm0);
+	_mm512_stream_si512((__m512i *)dest + 1, zmm1);
+	_mm512_stream_si512((__m512i *)dest + 2, zmm2);
+	_mm512_stream_si512((__m512i *)dest + 3, zmm3);
+	_mm512_stream_si512((__m512i *)dest + 4, zmm4);
+	_mm512_stream_si512((__m512i *)dest + 5, zmm5);
+	_mm512_stream_si512((__m512i *)dest + 6, zmm6);
+	_mm512_stream_si512((__m512i *)dest + 7, zmm7);
+	_mm512_stream_si512((__m512i *)dest + 8, zmm8);
+	_mm512_stream_si512((__m512i *)dest + 9, zmm9);
+	_mm512_stream_si512((__m512i *)dest + 10, zmm10);
+	_mm512_stream_si512((__m512i *)dest + 11, zmm11);
+	_mm512_stream_si512((__m512i *)dest + 12, zmm12);
+	_mm512_stream_si512((__m512i *)dest + 13, zmm13);
+	_mm512_stream_si512((__m512i *)dest + 14, zmm14);
+	_mm512_stream_si512((__m512i *)dest + 15, zmm15);
+}
+
+static inline void
+memmove_movnt8x64b(char *dest, const char *src)
+{
+	__m512i zmm0 = _mm512_loadu_si512((__m512i *)src + 0);
+	__m512i zmm1 = _mm512_loadu_si512((__m512i *)src + 1);
+	__m512i zmm2 = _mm512_loadu_si512((__m512i *)src + 2);
+	__m512i zmm3 = _mm512_loadu_si512((__m512i *)src + 3);
+	__m512i zmm4 = _mm512_loadu_si512((__m512i *)src + 4);
+	__m512i zmm5 = _mm512_loadu_si512((__m512i *)src + 5);
+	__m512i zmm6 = _mm512_loadu_si512((__m512i *)src + 6);
+	__m512i zmm7 = _mm512_loadu_si512((__m512i *)src + 7);
+
+	_mm512_stream_si512((__m512i *)dest + 0, zmm0);
+	_mm512_stream_si512((__m512i *)dest + 1, zmm1);
+	_mm512_stream_si512((__m512i *)dest + 2, zmm2);
+	_mm512_stream_si512((__m512i *)dest + 3, zmm3);
+	_mm512_stream_si512((__m512i *)dest + 4, zmm4);
+	_mm512_stream_si512((__m512i *)dest + 5, zmm5);
+	_mm512_stream_si512((__m512i *)dest + 6, zmm6);
+	_mm512_stream_si512((__m512i *)dest + 7, zmm7);
+}
+
+static inline void
 memmove_movnt4x64b(char *dest, const char *src)
 {
 	__m512i zmm0 = _mm512_loadu_si512((__m512i *)src + 0);
@@ -125,6 +329,16 @@ memmove_movnt4x64b(char *dest, const char *src)
 	_mm512_stream_si512((__m512i *)dest + 1, zmm1);
 	_mm512_stream_si512((__m512i *)dest + 2, zmm2);
 	_mm512_stream_si512((__m512i *)dest + 3, zmm3);
+}
+
+static inline void
+memmove_movnt2x64b(char *dest, const char *src)
+{
+	__m512i zmm0 = _mm512_loadu_si512((__m512i *)src + 0);
+	__m512i zmm1 = _mm512_loadu_si512((__m512i *)src + 1);
+
+	_mm512_stream_si512((__m512i *)dest + 0, zmm0);
+	_mm512_stream_si512((__m512i *)dest + 1, zmm1);
 }
 
 static inline void
@@ -168,11 +382,44 @@ measure(char *pmem, size_t pmemlen, size_t cpy_len, char *dram, bool csv)
 	for (unsigned i = 0; i < LOOPS; ++i) {
 		while (dest < dest_end) {
 			size_t sz = cpy_len;
+
+#if defined(USE_AVX512F) && defined(USE_ALL_REGS)
+			while (sz >= 2048) {
+				memmove_movnt32x64b(dest, src);
+				dest += 2048;
+				src += 2048;
+				sz -= 2048;
+			}
+
+			while (sz >= 1024) {
+				memmove_movnt16x64b(dest, src);
+				dest += 1024;
+				src += 1024;
+				sz -= 1024;
+			}
+#endif
+
+#if (defined(USE_AVX) || defined(USE_AVX512F)) && defined(USE_ALL_REGS)
+			while (sz >= 512) {
+				memmove_movnt8x64b(dest, src);
+				dest += 512;
+				src += 512;
+				sz -= 512;
+			}
+#endif
+
 			while (sz >= 256) {
 				memmove_movnt4x64b(dest, src);
 				dest += 256;
 				src += 256;
 				sz -= 256;
+			}
+
+			while (sz >= 128) {
+				memmove_movnt2x64b(dest, src);
+				dest += 128;
+				src += 128;
+				sz -= 128;
 			}
 
 			while (sz >= 64) {
